915ccbef27547914679f6b99abdee14d90aa4a76
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-10-17 15:22:29 +0300

    add frontend/src/services/

---
+++ frontend/src/services/i18nextInstance.js
@@ -0,0 +1,21 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import ru from '../locales/ru';
+
+const i18nextInstance = i18next.createInstance();
+
+export const initializeI18next = async () => {
+  await i18nextInstance.use(initReactI18next).init({
+    lng: 'ru', // Текущий язык
+    fallbackLng: 'ru',
+    debug: true,
+    resources: {
+      ru,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+};
+
+export default i18nextInstance;
---
+++ frontend/src/services/routes.js
@@ -0,0 +1,10 @@
+export default {
+  signUp: '/signup',
+  login: '/login',
+  home: '/',
+  api: {
+    base: '/api/v1',
+    channels: '/api/v1/channels',
+    messages: '/api/v1/messages',
+  },
+};
